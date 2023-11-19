2b225da993261f470759f52a5dadb55594f35bd8
jagdish47 <jagdishkumawat81@gmail.com>
2023-11-19 14:32:09 +0530

    useMutation - Create Update Delete in RQ

--- react-query/src/components/RQSuperHeroes.page.js
+++ react-query/src/components/RQSuperHeroes.page.js
@@ -1,7 +1,15 @@
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import { useState } from "react";
+
+import {
+  useSuperHeroesData,
+  useAddSuperHeroData,
+} from "../hooks/useSuperHeroesData";
 import { Link } from "react-router-dom";
 
 const RQSuperHeroesPage = () => {
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
+
   const onSuccess = (data) => {
     console.log("Perform side effect after data fetching", data);
   };
@@ -13,6 +21,8 @@ const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching, refetch } =
     useSuperHeroesData(onSuccess, onError);
 
+  const { mutate } = useAddSuperHeroData();
+
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
@@ -21,9 +31,30 @@ const RQSuperHeroesPage = () => {
     return <h2>{error.message} </h2>;
   }
 
+  const handleAddHeroClick = () => {
+    console.log({ name, alterEgo });
+
+    const hero = { name, alterEgo };
+    mutate(hero);
+  };
+
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
 
       <button onClick={refetch}>Fetch Heroes</button>
 
