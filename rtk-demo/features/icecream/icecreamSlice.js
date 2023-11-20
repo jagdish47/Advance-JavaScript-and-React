e50c23f320a1370082fecfbee43f75bcd348099a
jagdish47 <jagdishkumawat81@gmail.com>
2023-11-20 18:30:22 +0530

    Extra Reducer

--- rtk-demo/features/icecream/icecreamSlice.js
+++ rtk-demo/features/icecream/icecreamSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cakeSlice from "../cake/cakeSlice";
 
 const initialState = {
   numOfIcecream: 10,
@@ -15,6 +16,17 @@ const icecreamSlice = createSlice({
     restoke: (state, action) => {
       state.numOfIcecream += action.payload;
     },
+  },
+  //   extraReducers: {
+  //     ["cake/ordered"]: (state) => {
+  //       state.numOfIcecream--;
+  //     },
+  //   },
+
+  extraReducers: (builder) => {
+    builder.addCase(cakeSlice.ordered, (state) => {
+      state.numOfIcecream--;
+    });
   },
 });
 
