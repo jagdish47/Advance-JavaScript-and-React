7f6ecf14922dc5b10244c8cf88186e675a5a2e29
jagdish47 <jagdishkumawat81@gmail.com>
2023-12-13 12:04:50 +0530

    Dynamic Value Age and DOB

--- rhf-demo/src/components/YoutubeForm.tsx
+++ rhf-demo/src/components/YoutubeForm.tsx
@@ -1,10 +1,17 @@
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 type FormValues = {
   username: string;
   email: string;
   channel: string;
+  social: { twitter: string; facebook: string };
+  phoneNumbers: string[];
+  phNumbers: {
+    number: string;
+  }[];
+  age: number;
+  dob: Date;
 };
 
 const YoutubeForm = () => {
@@ -13,10 +20,23 @@ const YoutubeForm = () => {
       username: "Jagdish Kumawat",
       email: "jagdishkumawat81@gmial.com",
       channel: "Codevalution",
+      social: {
+        twitter: "",
+        facebook: "",
+      },
+      phoneNumbers: ["", ""],
+      phNumbers: [{ number: "" }],
+      age: 0,
+      dob: new Date(),
     },
   });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
+
+  const { fields, append, remove } = useFieldArray({
+    name: "phNumbers",
+    control,
+  });
 
   const onSubmit = (data: FormValues) => {
     console.log("Data : ", data);
@@ -60,6 +80,7 @@ const YoutubeForm = () => {
             },
           })}
         />
+
         <p>{errors.email?.message}</p>
 
         <label>Channel</label>
@@ -70,6 +91,81 @@ const YoutubeForm = () => {
           {...register("channel")}
         />
         <p>{errors.channel?.message}</p>
+
+        <label>DOB</label>
+        <input
+          type="date"
+          id="date"
+          placeholder="Date of Birth"
+          {...register("dob", {
+            valueAsDate: true,
+          })}
+        />
+        <p>{errors.dob?.message}</p>
+
+        <label>Age</label>
+        <input
+          type="number"
+          id="age"
+          placeholder="age"
+          {...register("age", {
+            valueAsNumber: true,
+          })}
+        />
+        <p>{errors.age?.message}</p>
+
+        <label>Facebook</label>
+        <input
+          type="text"
+          placeholder="Facebook"
+          {...register("social.facebook")}
+        />
+
+        <label>Twitter</label>
+        <input
+          type="text"
+          placeholder="Twitter"
+          {...register("social.twitter", {
+            required: "Twitter Url is Required",
+          })}
+        />
+
+        <label>Primary Number</label>
+        <input
+          type="text"
+          placeholder="Primary Number"
+          {...register("phoneNumbers.0", {
+            required: "Primary Number is Required",
+          })}
+        />
+        <label>Secondary Number</label>
+        <input
+          type="text"
+          placeholder="Secondary Number"
+          {...register("phoneNumbers.1")}
+        />
+
+        <div>
+          <label>List of Phone numbers</label>
+          <div>
+            {fields.map((field, index) => {
+              return (
+                <div>
+                  <input
+                    type="text"
+                    {...register(`phNumbers.${index}.number`)}
+                  />
+                  <button type="button" onClick={() => remove(index)}>
+                    Remove
+                  </button>
+                </div>
+              );
+            })}
+            <button onClick={() => append({ number: "" })} type="button">
+              Add
+            </button>
+          </div>
+        </div>
 
         <button>Submit</button>
       </form>
